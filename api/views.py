bc858658bd7e7e87c4f7fd94cd845a5decd8805e
Kevin Ochieng <kevincliffo@gmail.com>
2020-11-28 23:43:22 +0300

    Adding permissions

--- api/views.py
+++ api/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from .models import Exam, Student
 from .serializers import ExamsSerializer, StudentsSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 class StudentsViewSet(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentsSerializer
-
+    permission_classes = (IsAuthenticated,)
+    
 class ExamsViewSet(ModelViewSet):
     queryset = Exam.objects.all()
     serializer_class = ExamsSerializer
